fda9ab23ea831b7022435fedf1f576c0b3d4864b
Pearl <rkdwlswn227@gmail.com>
2024-07-21 09:55:20 +0900

    refactor: 파일 경로, 세부 수정

--- app/src/main/java/com/example/readme/ui/MainActivity.kt
+++ app/src/main/java/com/example/readme/ui/MainActivity.kt
@@ -1,7 +1,6 @@
-package com.example.readme
+package com.example.readme.ui
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
