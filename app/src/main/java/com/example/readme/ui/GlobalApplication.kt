ab2e4ee9bb9a279fa55c38b16fefed1343b9fa40
OSSU <o.ss.u2056@gmail.com>
2024-08-18 01:12:50 +0900

    Fix: 메인 런처 변경

--- app/src/main/java/com/example/readme/ui/GlobalApplication.kt
+++ app/src/main/java/com/example/readme/ui/GlobalApplication.kt
@@ -11,7 +11,7 @@ class GlobalApplication : Application() {
         // 다른 초기화 코드들
 
         // Kakao SDK 초기화
-        KakaoSdk.init(this, "dbe31c51bcd2bae3fbab42016106e896")
+        KakaoSdk.init(this, "8bd1ca39d5eb15687ae52deb301f1abe")
 
         // 초기화 시 RetrofitClient 설정
         val apiService = RetrofitClient.getReadmeServerService()
