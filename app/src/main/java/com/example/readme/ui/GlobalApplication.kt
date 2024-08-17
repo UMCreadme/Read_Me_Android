824efe27bdbf08d347d30140e654908c1a56f8ea
bleuxsy <100823803+bleuxsy@users.noreply.github.com>
2024-08-17 21:02:13 +0900

    start activity 연결 완료
    여기에 해시키는 각자 컴퓨터에서 따로 받아서 설정해야할 것 같아요!

--- app/src/main/java/com/example/readme/ui/GlobalApplication.kt
+++ app/src/main/java/com/example/readme/ui/GlobalApplication.kt
@@ -1,7 +1,9 @@
 package com.example.readme.ui
 
 import android.app.Application
+import android.util.Log
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.sdk.common.util.Utility
 
 class GlobalApplication : Application() {
     override fun onCreate() {
@@ -9,6 +11,9 @@ class GlobalApplication : Application() {
         // 다른 초기화 코드들
 
         // Kakao SDK 초기화
-        KakaoSdk.init(this, "dbe31c51bcd2bae3fbab42016106e896")
+//        KakaoSdk.init(this, "dbe31c51bcd2bae3fbab42016106e896")
+        KakaoSdk.init(this, "8bd1ca39d5eb15687ae52deb301f1abe")
+        Log.e("hashKey", Utility.getKeyHash(this))
+
     }
 }
